fe01f1ef8c0338153be73349675f98d7cde5249e
Rob Johnston <clemsonjeeper@gmail.com>
2014-03-27 16:55:40 -0400

    * Add SyndicationExampleApp skeleton project

---
+++ SyndicationExampleApp/SyndicationExampleApp/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  SyndicationExampleApp
+//
+//  Copyright (c) 2014 CTAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
